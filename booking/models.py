b0a41a859d0184be6e4e3be8c6613870b60ed43a
BubiniB <cristina.ekqvist@outlook.com>
2023-10-16 07:57:04 +0000

    Create customer model

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+# Customers
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50, null=False, blank=False)
+    last_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(db_index=True, unique=True, null=False, blank=False)
+    phone = PhoneNumberField(null=False, blank=False)
