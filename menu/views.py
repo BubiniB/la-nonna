c7a6a3ce79bacc434f78b8380f13a11650d9daf3
BubiniB <cristina.ekqvist@outlook.com>
2023-11-05 13:57:53 +0000

    Bugfix: Specify template location in menu view

--- menu/views.py
+++ menu/views.py
@@ -6,4 +6,4 @@ from .models import Menu
 # Only one view because there aren't many items for each course 
 def menu(request):
     menu_items = Menu.objects.all()
-    return render(request, 'menu.html', {'menu_items': menu_items})
+    return render(request, 'menu/menu.html', {'menu_items': menu_items})
