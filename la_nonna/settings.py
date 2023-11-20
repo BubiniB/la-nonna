c7dd14694f33df93c2e1fbbcf6a8a3914010e8bf
BubiniB <cristina.ekqvist@outlook.com>
2023-11-20 20:29:30 +0000

    Chang back settings

--- la_nonna/settings.py
+++ la_nonna/settings.py
@@ -141,11 +141,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
-]
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_URL = '/media/'
